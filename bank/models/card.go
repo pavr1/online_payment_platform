e9ba2221e723359b71d8fbd62df2b7c67160534d
Pablo Villalobos <pavr1@hotmail.com>
2024-08-27 16:54:54 -0600

    Adding customer data into card

--- bank/models/card.go
+++ bank/models/card.go
@@ -1,12 +1,13 @@
 package models
 
 type Card struct {
-	ID         string   `json:"id"`
-	CardNumber string   `json:"card_number"`
-	HolderName string   `json:"holder_name"`
-	ExpDate    string   `json:"exp_date"`
-	CVV        string   `json:"cvv"`
-	account    *Account `json:"account"`
+	ID         string    `json:"id"`
+	CardNumber string    `json:"card_number"`
+	HolderName string    `json:"holder_name"`
+	ExpDate    string    `json:"exp_date"`
+	CVV        string    `json:"cvv"`
+	account    *Account  `json:"account"`
+	customer   *Customer `json:"customer"`
 }
 
 func (c *Card) GetAmount() float64 {
@@ -15,4 +16,8 @@ func (c *Card) GetAmount() float64 {
 
 func (c *Card) SetAmount(amount float64) {
 	c.account.Amount = amount
+}
+
+func (c *Card) GetCustomerName() string {
+	return c.customer.FirstName + " " + c.customer.LastName
 }
